fb6fb4ee89b121976facfc206b9ff620e443eab8
GarofaloD <garofalodaniel@gmail.com>
2025-09-14 17:06:56 -0500

    chore: first init

---
+++ src/main/java/com/garofalo/fearladderhealthcheck/FearladderhealthcheckApplication.java
@@ -0,0 +1,13 @@
+package com.garofalo.fearladderhealthcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FearladderhealthcheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FearladderhealthcheckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garofalo/fearladderhealthcheck/FearladderhealthcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garofalo.fearladderhealthcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FearladderhealthcheckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
